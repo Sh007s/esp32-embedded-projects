e48d00666dc305c1c76766178bf58799f7e82d76
Shankar S <shankar007skr@gmail.com>
2026-06-27 23:38:31 +0530

    Complete text rendering module with alignment, scaling, and printf support

--- prototypes/esp32/tft_display/tft_text_rendrening/main/TFT_Custom_Project.c
+++ prototypes/esp32/tft_display/tft_text_rendrening/main/TFT_Custom_Project.c
@@ -9,55 +9,65 @@ void app_main(void)
 
     display_fill(TFT_BLACK);
 
-    /* Normal text */
-    display_draw_string(20, 20, "display_draw_string()", TFT_WHITE);
+    // /* Normal text */
+    // display_draw_string(20, 20, "display_draw_string()", TFT_WHITE);
 
-    /* Background text */
-    display_draw_string_bg(40, 20, "display_draw_string_bg()", TFT_WHITE, TFT_RED);
+    // /* Background text */
+    // display_draw_string_bg(40, 20, "display_draw_string_bg()", TFT_WHITE, TFT_RED);
 
-    /* Scaled text */
-    display_draw_string_scaled(60, 20, "Scale 1", TFT_GREEN, 1);
+    // /* Scaled text */
+    // display_draw_string_scaled(60, 20, "Scale 1", TFT_GREEN, 1);
 
-    display_draw_string_scaled(80, 20, "Scale 2", TFT_YELLOW, 2);
+    // display_draw_string_scaled(80, 20, "Scale 2", TFT_YELLOW, 2);
 
-    display_draw_string_scaled(100, 20, "Scale 3", TFT_CYAN, 3);
+    // display_draw_string_scaled(100, 20, "Scale 3", TFT_CYAN, 3);
 
-    /* Character width */
-    uint16_t char_width = display_get_char_width();
+    // /* Character width */
+    // uint16_t char_width = display_get_char_width();
 
-    display_draw_string(130, 20, "Char Width:", TFT_WHITE);
+    // display_draw_string(130, 20, "Char Width:", TFT_WHITE);
 
-    display_draw_char(130, 130, '0' + char_width, TFT_GREEN);
+    // display_draw_char(130, 130, '0' + char_width, TFT_GREEN);
 
-    /* Character height */
-    uint16_t char_height = display_get_char_height();
+    // /* Character height */
+    // uint16_t char_height = display_get_char_height();
 
-    display_draw_string(150, 20, "Char Height:", TFT_WHITE);
+    // display_draw_string(150, 20, "Char Height:", TFT_WHITE);
 
-    display_draw_char(150, 140, '0' + char_height, TFT_GREEN);
+    // display_draw_char(150, 140, '0' + char_height, TFT_GREEN);
 
-    /* String width */
-    uint16_t string_width = display_get_string_width("HELLO");
+    // /* String width */
+    // uint16_t string_width = display_get_string_width("HELLO");
 
-    display_draw_string(180, 20, "HELLO Width:", TFT_WHITE);
+    // display_draw_string(180, 20, "HELLO Width:", TFT_WHITE);
 
-    if (string_width >= 10)
-    {
-        display_draw_char(180, 135, '0' + (string_width / 10), TFT_GREEN);
+    // if (string_width >= 10)
+    // {
+    //     display_draw_char(180, 135, '0' + (string_width / 10), TFT_GREEN);
 
-        display_draw_char(180, 141, '0' + (string_width % 10), TFT_GREEN);
-    }
-    else
-    {
-        display_draw_char(180, 141, '0' + string_width, TFT_GREEN);
-    }
+    //     display_draw_char(180, 141, '0' + (string_width % 10), TFT_GREEN);
+    // }
+    // else
+    // {
+    //     display_draw_char(180, 141, '0' + string_width, TFT_GREEN);
+    // }
 
-    /* Center aligned */
-    display_draw_string_center(120, 180, "CENTER", TFT_MAGENTA);
+    // /* Center aligned */
+    // display_draw_string_center(120, 180, "CENTER", TFT_MAGENTA);
 
-    /* Right aligned */
-    display_draw_string_right(220, 275, "RIGHT", TFT_BLUE);
+    // /* Right aligned */
+    // display_draw_string_right(220, 275, "RIGHT", TFT_BLUE);
 
+    display_printf(20, 20, TFT_WHITE, "HELLO TFT");
+
+    display_printf(40, 20, TFT_RED, "Speed : %d km/h", 80);
+
+    display_printf(60, 20, TFT_GREEN, "Temp : %.1f C", 27.5);
+
+    display_printf(80, 20, TFT_CYAN, "Battery : %d%%", 95);
+
+    display_printf(100, 20, TFT_YELLOW, "Version : %s", "V1.0");
+    
     while (1)
     {
         vTaskDelay(pdMS_TO_TICKS(1000));
